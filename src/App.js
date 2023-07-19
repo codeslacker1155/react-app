8b1c10f21aae5dcba7fdd39277f63ffcb87a1e5f
chyatt1155 <christopher.hyatt@entersoft.com.au>
2023-07-19 00:43:40 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import BookSearchPage from './components/BookSearchPage';
-import BookDetailsPage from './components/BookDetailsPage';
-import PublicBookshelfPage from './components/PublicBookshelfPage';
+import BookSearchPage from '/components/BookSearchPage';
+import BookDetailsPage from '/components/BookDetailsPage';
+import PublicBookshelfPage from '/components/PublicBookshelfPage';
 import './index.css';
 
 function App() {
