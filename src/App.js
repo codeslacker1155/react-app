3fd9f38f398bd7b14c40e7a254bb917aacf5676a
chyatt1155 <christopher.hyatt@entersoft.com.au>
2023-07-18 21:26:19 -0400

    update

--- src/App.js
+++ src/App.js
@@ -20,61 +20,42 @@ function App() {
 
   return (
     <div className="App">
-      <header>
-      <input 
+      <header className="header">
+        <input 
+          className="search-input"
           type="text" 
           value={search} 
           onChange={e => setSearch(e.target.value)} 
           placeholder="Search for books" 
         />
-        <button onClick={handleSearch}>Search</button>
+        <button className="search-button" onClick={handleSearch}>Search</button>
       </header>
 
       <main>
-        <div>
-          <h2>{book.volumeInfo?.title}</h2>
-          <p>{book.volumeInfo?.description || 'No description available'}</p>
+        <div className="book">
+          <h2 className="book-title">{book.volumeInfo?.title}</h2>
+          <img className="book-image" src={book.volumeInfo?.imageLinks?.thumbnail} alt={book.volumeInfo?.title} />
+          <p className="book-description">{book.volumeInfo?.description || 'No description available'}</p>
         </div>
 
         {books.map(book => (
-          <div key={book.id}>
-            <h2>{book.volumeInfo.title}</h2>
-            <p>{book.volumeInfo.description || 'No description available'}</p>
-            <p>
-              {book.volumeInfo.authors?.map(author => (
-                <span key={author}>{author}</span>
-              ))}
-
-              {book.volumeInfo.authors?.length === 0 && <span>No authors available</span>}
-
-              {book.volumeInfo.authors?.length > 0 && <span>Authors available</span>}
-            </p>
-            <p>
-              {book.volumeInfo.categories?.map(category => (
-                <span key={category}>{category}</span>
-              ))}
-
-              {book.volumeInfo.categories?.length === 0 && <span>No categories available</span>}
-            </p>
-            <p>
-              {book.volumeInfo.industryIdentifiers?.map(identifier => (
-                <span key={identifier.identifier}>{identifier.identifier}</span>
-              ))}
-
-              {book.volumeInfo.industryIdentifiers?.length === 0 && <span>No identifiers available</span>}
-            </p>
-            <p>{book.volumeInfo.publishedDate}</p>
-            <p>{book.volumeInfo.publisher}</p>
-            <p>{book.volumeInfo.pageCount}</p>
-            <p>{book.volumeInfo.averageRating}</p>
-            <p>{book.volumeInfo.ratingsCount}</p>
-            <p>{book.volumeInfo.maturityRating}</p>
-            <p>{book.volumeInfo.language}</p>
-            <p>{book.volumeInfo.previewLink}</p>
-            <p>{book.volumeInfo.infoLink}</p>
-            <p>{book.volumeInfo.canonicalVolumeLink}</p>
-            <p>{book.volumeInfo.subtitle}</p>
-            <img src={book.volumeInfo.imageLinks?.thumbnail} alt={book.volumeInfo.title} />
+          <div className="book" key={book.id}>
+            <h2 className="book-title">{book.volumeInfo.title}</h2>
+            <img className="book-image" src={book.volumeInfo.imageLinks?.thumbnail} alt={book.volumeInfo.title} />
+            <p className="book-description">{book.volumeInfo.description || 'No description available'}</p>
+            <p className="book-authors">Authors: {book.volumeInfo.authors?.join(', ') || 'No authors available'}</p>
+            <p className="book-categories">Categories: {book.volumeInfo.categories?.join(', ') || 'No categories available'}</p>
+            <p className="book-identifiers">Identifiers: {book.volumeInfo.industryIdentifiers?.map(identifier => identifier.identifier).join(', ') || 'No identifiers available'}</p>
+            <p className="book-published-date">Published Date: {book.volumeInfo.publishedDate}</p>
+            <p className="book-publisher">Publisher: {book.volumeInfo.publisher}</p>
+            <p className="book-page-count">Page Count: {book.volumeInfo.pageCount}</p>
+            <p className="book-average-rating">Average Rating: {book.volumeInfo.averageRating}</p>
+            <p className="book-ratings-count">Ratings Count: {book.volumeInfo.ratingsCount}</p>
+            <p className="book-language">Language: {book.volumeInfo.language}</p>
+            <p className="book-preview-link">Preview Link: <a href={book.volumeInfo.previewLink}>{book.volumeInfo.previewLink}</a></p>
+            <p className="book-info-link">Info Link: <a href={book.volumeInfo.infoLink}>{book.volumeInfo.infoLink}</a></p>
+            <p className="book-canonical-volume-link">Canonical Volume Link: <a href={book.volumeInfo.canonicalVolumeLink}>{book.volumeInfo.canonicalVolumeLink}</a></p>
+            <p className="book-subtitle">Subtitle: {book.volumeInfo.subtitle}</p>
             <hr />
           </div>
         ))}
