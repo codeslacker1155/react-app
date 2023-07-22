015eb71427c872541981b460e89586d125aff143
codeslacker1155 <cthyatt710@gmail.com>
2023-07-22 00:58:50 -0400

    update

--- src/components/BookDetailsPage.js
+++ src/components/BookDetailsPage.js
@@ -21,14 +21,14 @@ function BookDetailsPage({ id }) { // Receive id as a prop
 
     // Replace carriage return characters with line breaks
     const description = book.volumeInfo.description.replace(/_x000D_/g, '<br />');
-    const isbn = volumeInfo.industryIdentifiers.find(identifier => identifier.type === 'ISBN_13' || identifier.type === 'ISBN_10');
+    const isbn = book.volumeInfo.industryIdentifiers.find(identifier => identifier.type === 'ISBN_13' || identifier.type === 'ISBN_10');
 
     const template = `
       <div class="book">
         <h2 class="book-title">{{volumeInfo.title}}</h2>
         <img class="book-image" src="{{volumeInfo.imageLinks.thumbnail}}" alt="{{volumeInfo.title}}" />
         <div class="book-description">${description}</div>
-        <p class="book-info"><strong>ISBN: </strong>{{isbn.identifier}}</p>
+        <p class="book-info"><strong>ISBN: </strong>${isbn ? isbn.identifier : 'N/A'}</p>
         <p class="book-info"><strong>Author(s): </strong>{{#volumeInfo.authors}}{{.}}{{/volumeInfo.authors}}</p>
         <p class="book-info"><strong>Publisher: </strong>{{volumeInfo.publisher}}</p>
         <p class="book-info"><strong>Page Count: </strong>{{volumeInfo.pageCount}}</p>
